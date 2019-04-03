e4104b3b2adf154794dd6a29b4af26fc7148f399
piokowalski <piokowalski@onet.pl>
2019-04-03 09:37:03 +0200

    Anonymous class (print$) (number)

--- cwiczenia/src/main/java/Main.java
+++ cwiczenia/src/main/java/Main.java
@@ -3,7 +3,17 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Task o = new MyTask();
+//        Task o = new MyTask();
+//        carryOutWork(o);
+
+        Task o = new Task() {
+            @Override
+            public void doWork() {
+                System.out.println("Hello from " +
+                        getClass().getName());
+            }
+        };
+
         carryOutWork(o);
 
 
